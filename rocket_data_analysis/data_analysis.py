e435bb3e3a28136c3bae6cbee3e7e4f98aa76401
shaharshu <shaharshu1000@gmail.com>
2024-11-22 10:43:28 +0200

    New graphic branch

--- rocket_data_analysis/data_analysis.py
+++ rocket_data_analysis/data_analysis.py
@@ -7,7 +7,7 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-# change again 3
+# change again 4
 # Function to generate more realistic random data
 def generate_realistic_data(iteration):
     # Simulate realistic changes over time
